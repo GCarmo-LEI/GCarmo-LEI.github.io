7971c66f2b21c93d085314512fc2fad90886d1fd
GCarmo-LEI <a22305125@alunos.ulht.pt>
2024-11-21 09:37:57 +0000

    updated

--- Lab8/main.js
+++ Lab8/main.js
@@ -6,14 +6,24 @@ document.getElementById("hoverText").addEventListener("mouseout", function() {
     this.textContent = "Passa por aqui!";
 });
 
-function changeColor(color) {
-    document.getElementById("colorText").style.color = color;
-}
+const corCSS = {
+    vermelho: "red",
+    verde: "green",
+    azul: "blue"
+};
 
-function randomizeInputColor() {
+document.querySelectorAll("button[data-color]").forEach(button => {
+    button.addEventListener("click", () => {
+        const cor = button.dataset.color; 
+        const corEmCSS = corCSS[cor];
+        document.getElementById("colorText").style.color = corEmCSS; 
+    });
+});
+
+const randomizeInputColor = () => {
     const colorChangingInput = document.getElementById("colorChangingInput");
     colorChangingInput.style.backgroundColor = getRandomColor();
-}
+};
 
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
@@ -24,9 +34,9 @@ function getRandomColor() {
     return color;
 }
 
-function changeBackgroundColor() {
-    const colorInput = document.getElementById("colorInput").value.toLowerCase();
-    document.body.style.backgroundColor = colorInput;
+function changeBackgroundColor(element) {
+    const selectedColor = element.value.toLowerCase();
+    document.body.style.backgroundColor = selectedColor;
 }
 
 let count = 0;
@@ -34,3 +44,23 @@ function incrementCounter() {
     count++;
     document.getElementById("counter").textContent = count;
 }
+
+document.querySelector('form').onsubmit = (e) => {
+    e.preventDefault();
+    
+    const name = document.querySelector('#name').value;
+    const age = document.querySelector('#age').value;
+    
+    const message = document.querySelector('#message');
+    message.textContent = `Olá, o ${name} tem ${age}!`;
+};
+
+let autoCounterValue = 0;
+
+function updateAutoCounter() {
+    autoCounterValue++;
+    document.getElementById("autoCounter").textContent = autoCounterValue;
+}
+
+// Inicia o incremento automático
+setInterval(updateAutoCounter, 1000);
